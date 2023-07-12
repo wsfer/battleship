0584fa854de39f6a89b2998c6c78d34a3658f79a
wsfer <93054415+wsfer@users.noreply.github.com>
2023-07-11 22:02:13 -0300

    move DOM manupulation logic to game controller

--- src/scripts/gameController.js
+++ src/scripts/gameController.js
@@ -1,8 +1,23 @@
+// Play turns with players and change some DOM content after each turn
 class GameController {
     #players;
 
-    constructor(playerOne, playerTwo) {
+    #DOMContent;
+
+    // Player squares on DOM should come here as a matrix on DOMContent object
+    constructor(playerOne, playerTwo, DOMContent) {
         this.#players = [playerTwo, playerOne];
+        this.#DOMContent = {
+            combatLog: DOMContent.combatLog,
+            nextPlayer: DOMContent.nextPlayer,
+            winnerName: DOMContent.winnerName,
+        };
+        DOMContent.combatLog.textContent = 'Nothing is happening';
+        DOMContent.nextPlayer.textContent = playerOne.name;
+
+        // This assign makes it easy to change squares on different turns
+        playerOne.squares = DOMContent.playerOneSquares;
+        playerTwo.squares = DOMContent.playerTwoSquares;
     }
 
     get nextPlayer() {
@@ -14,6 +29,21 @@ class GameController {
         const defender = this.#players.pop();
         const attack = await attacker.attack(defender, coords);
         this.#players.push(attacker, defender);
+
+        // If it's a sunken ship nothing will happen
+        if (attack.isShip) {
+            defender.squares[attack.x][attack.y].classList.toggle('sunken');
+        } else if (attack.target === 'Water') {
+            defender.squares[attack.x][attack.y].classList.toggle('water');
+        }
+
+        this.#DOMContent.combatLog.textContent = `${attacker.name} attacked ${defender.name}'s ${attack.target}`;
+        this.#DOMContent.nextPlayer.textContent = defender.name;
+
+        if (attack.gameover) {
+            this.#DOMContent.winnerName.textContent = attacker.name;
+        }
+
         return {
             attacker: attacker.name,
             defender: defender.name,
--- src/scripts/gameController.test.js
+++ src/scripts/gameController.test.js
@@ -1,38 +1,152 @@
 import GameController from './gameController';
 
-test('Returns correct values and pass turns', async () => {
-    const player1 = {
+const mockDOMElement = function DOMElementFactory() {
+    const elementClasses = new Set();
+    const add = (name) => elementClasses.add(name);
+    const remove = (name) => elementClasses.delete(name);
+    const contains = (name) => elementClasses.has(name);
+    const toggle = (name) =>
+        elementClasses.has(name)
+            ? elementClasses.delete(name)
+            : elementClasses.add(name);
+
+    return { textContent: '', classList: { add, remove, contains, toggle } };
+};
+
+let player1;
+let player2;
+let DOMContent;
+
+beforeEach(() => {
+    player1 = {
         name: 'Player_one',
         attack(player, coords) {
             return player.receiveAttack(coords);
         },
-        receiveAttack: jest.fn().mockReturnValue({ infos: 'Player1 attacked' }),
+        receiveAttack: jest.fn().mockReturnValue({
+            target: 'Destroyer',
+            x: 5,
+            y: 0,
+            isShip: true,
+            gameover: false,
+        }),
     };
 
-    const player2 = {
+    player2 = {
         name: 'SeCoNd_PlAyEr',
         attack(player, coords) {
             return player.receiveAttack(coords);
         },
-        receiveAttack: jest.fn().mockReturnValue({ infos: 'Player2 attacked' }),
+        receiveAttack: jest
+            .fn()
+            .mockReturnValueOnce({
+                target: 'Water',
+                x: 8,
+                y: 1,
+                isShip: false,
+                gameover: false,
+            })
+            .mockReturnValue({
+                target: 'Carrier',
+                x: 1,
+                y: 1,
+                isShip: true,
+                gameover: true,
+            }),
     };
 
-    const game = new GameController(player1, player2);
-    const turn = await game.play([3, 4]);
+    const squares1 = [[], [], [], [], [], [], [], [], [], []];
+    const squares2 = [[], [], [], [], [], [], [], [], [], []];
 
+    squares1[5][0] = mockDOMElement();
+    squares2[8][1] = mockDOMElement();
+    squares2[1][1] = mockDOMElement();
+
+    DOMContent = {
+        combatLog: mockDOMElement(),
+        nextPlayer: mockDOMElement(),
+        winnerName: mockDOMElement(),
+        playerOneSquares: squares1,
+        playerTwoSquares: squares2,
+    };
+});
+
+test('Pass turns', async () => {
+    const game = new GameController(player1, player2, DOMContent);
+    expect(game.nextPlayer).toBe(player1);
+    const turn = await game.play([9, 9]);
+    expect(game.nextPlayer).toBe(player2);
+    const newTurn = await game.play(null);
+    expect(game.nextPlayer).toBe(player1);
+});
+
+test('Returns correct values', async () => {
+    const game = new GameController(player1, player2, DOMContent);
+    const turn = await game.play([8, 1]);
     expect(turn).toEqual({
         attacker: 'Player_one',
         defender: 'SeCoNd_PlAyEr',
-        infos: 'Player2 attacked',
+        target: 'Water',
+        x: 8,
+        y: 1,
+        isShip: false,
+        gameover: false,
     });
-    expect(game.nextPlayer).toBe(player2);
 
     const newTurn = await game.play(null);
-
     expect(newTurn).toEqual({
         attacker: 'SeCoNd_PlAyEr',
         defender: 'Player_one',
-        infos: 'Player1 attacked',
+        target: 'Destroyer',
+        x: 5,
+        y: 0,
+        isShip: true,
+        gameover: false,
     });
-    expect(game.nextPlayer).toBe(player1);
+
+    const lastTurn = await game.play(null);
+    expect(lastTurn).toEqual({
+        attacker: 'Player_one',
+        defender: 'SeCoNd_PlAyEr',
+        target: 'Carrier',
+        x: 1,
+        y: 1,
+        isShip: true,
+        gameover: true,
+    });
+});
+
+test('Changes DOM content correctly', async () => {
+    const game = new GameController(player1, player2, DOMContent);
+
+    expect(DOMContent.nextPlayer.textContent).toBe('Player_one');
+
+    await game.play([8, 1]);
+    expect(DOMContent.combatLog.textContent).toBe(
+        "Player_one attacked SeCoNd_PlAyEr's Water"
+    );
+    expect(DOMContent.nextPlayer.textContent).toBe('SeCoNd_PlAyEr');
+    expect(DOMContent.winnerName.textContent).toBe('');
+    expect(DOMContent.playerTwoSquares[8][1].classList.contains('water'))
+        .toBeTruthy;
+
+    await game.play(null);
+
+    expect(DOMContent.combatLog.textContent).toBe(
+        "SeCoNd_PlAyEr attacked Player_one's Destroyer"
+    );
+    expect(DOMContent.nextPlayer.textContent).toBe('Player_one');
+    expect(DOMContent.winnerName.textContent).toBe('');
+    expect(DOMContent.playerOneSquares[5][0].classList.contains('sunken'))
+        .toBeTruthy;
+
+    await game.play('anything');
+
+    expect(DOMContent.combatLog.textContent).toBe(
+        "Player_one attacked SeCoNd_PlAyEr's Carrier"
+    );
+    expect(DOMContent.nextPlayer.textContent).toBe('SeCoNd_PlAyEr');
+    expect(DOMContent.winnerName.textContent).toBe('Player_one');
+    expect(DOMContent.playerTwoSquares[1][1].classList.contains('sunken'))
+        .toBeTruthy;
 });
