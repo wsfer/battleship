a969d52a92888d12c0ededcd7f44dbfed7b1e668
wsfer <93054415+wsfer@users.noreply.github.com>
2023-07-11 23:46:59 -0300

    Change font size on sound confirmation popup box

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
